0871bec509b8ccd62c8803be19e94d39c30b7c34
iamshakur <salmanshakur7@gmail.com>
2024-02-03 12:54:30 +0100

    Added logo to the input field

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,72 +1,81 @@
 function togglePasswordVisibility1() {
-    var passwordField = document.getElementById("passwordField1");
-    var eyeIcon = document.getElementById("eyeIcon1");
-  
-    if (passwordField.type === "password") {
-      passwordField.type = "text";
-      eyeIcon.classList.remove("fa-eye");
-      eyeIcon.classList.add("fa-eye-slash");
-    } else {
-      passwordField.type = "password";
-      eyeIcon.classList.remove("fa-eye-slash");
-      eyeIcon.classList.add("fa-eye");
-    }
-  }
+	var passwordField = document.getElementById("passwordField1");
+	var eyeIcon = document.getElementById("eyeIcon1");
 
-  function togglePasswordVisibility2() {
-    var passwordField = document.getElementById("passwordField2");
-    var eyeIcon = document.getElementById("eyeIcon2");
-  
-    if (passwordField.type === "password") {
-      passwordField.type = "text";
-      eyeIcon.classList.remove("fa-eye");
-      eyeIcon.classList.add("fa-eye-slash");
-    } else {
-      passwordField.type = "password";
-      eyeIcon.classList.remove("fa-eye-slash");
-      eyeIcon.classList.add("fa-eye");
-    }
-  }
+	if (passwordField.type === "password") {
+		passwordField.type = "text";
+		eyeIcon.classList.remove("fa-eye");
+		eyeIcon.classList.add("fa-eye-slash");
+	} else {
+		passwordField.type = "password";
+		eyeIcon.classList.remove("fa-eye-slash");
+		eyeIcon.classList.add("fa-eye");
+	}
+}
 
-  function togglePasswordVisibility() {
-    var passwordField = document.getElementById("passwordField");
-    var eyeIcon = document.getElementById("eyeIcon");
-  
-    if (passwordField.type === "password") {
-      passwordField.type = "text";
-      eyeIcon.classList.remove("fa-eye");
-      eyeIcon.classList.add("fa-eye-slash");
-    } else {
-      passwordField.type = "password";
-      eyeIcon.classList.remove("fa-eye-slash");
-      eyeIcon.classList.add("fa-eye");
-    }
-  }
+function togglePasswordVisibility2() {
+	var passwordField = document.getElementById("passwordField2");
+	var eyeIcon = document.getElementById("eyeIcon2");
 
- 
+	if (passwordField.type === "password") {
+		passwordField.type = "text";
+		eyeIcon.classList.remove("fa-eye");
+		eyeIcon.classList.add("fa-eye-slash");
+	} else {
+		passwordField.type = "password";
+		eyeIcon.classList.remove("fa-eye-slash");
+		eyeIcon.classList.add("fa-eye");
+	}
+}
 
-  document.addEventListener("DOMContentLoaded", function() {
-    const steps = document.querySelectorAll('.step');
-  
-    function updateProgress() {
-      let currentStep = 0;
-  
-      setInterval(() => {
-        steps.forEach((step) => {
-          step.classList.remove('active');
-        });
-  
-        if (currentStep < steps.length) {
-          steps[currentStep].classList.add('active');
-          currentStep++;
-        } else {
-          clearInterval();
-        }
-      }, 2000); // Adjust the duration as needed (in milliseconds)
-    }
-  
-    updateProgress(); // Start the progress update
-  });
-  
+function togglePasswordVisibility() {
+	var passwordField = document.getElementById("passwordField");
+	var eyeIcon = document.getElementById("eyeIcon");
 
-  
+	if (passwordField.type === "password") {
+		passwordField.type = "text";
+		eyeIcon.classList.remove("fa-eye");
+		eyeIcon.classList.add("fa-eye-slash");
+	} else {
+		passwordField.type = "password";
+		eyeIcon.classList.remove("fa-eye-slash");
+		eyeIcon.classList.add("fa-eye");
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	const steps = document.querySelectorAll(".step");
+
+	function updateProgress() {
+		let currentStep = 0;
+
+		setInterval(() => {
+			steps.forEach((step) => {
+				step.classList.remove("active");
+			});
+
+			if (currentStep < steps.length) {
+				steps[currentStep].classList.add("active");
+				currentStep++;
+			} else {
+				clearInterval();
+			}
+		}, 2000); // Adjust the duration as needed (in milliseconds)
+	}
+
+	updateProgress(); // Start the progress update
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	const input = document.querySelector("#phone");
+	window.intlTelInput(input, {
+		utilsScript:
+			"https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
+	});
+
+	const iti = window.intlTelInput(input, {
+		utilsScript:
+			"https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
+		initialCountry: "ng", // "ng" is the ISO country code for Nigeria
+	});
+});
